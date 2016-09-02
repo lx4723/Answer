b7188727dcfe26d2be0b7fb7185a9918a0ec8f98
lixing <lixing@cloudpioneer.com.cn>
2016-09-02 16:41:14 +0800

    单元测试

---
+++ src/test/java/com/answer/service/Impl/QuestionServiceImplTest.java
@@ -0,0 +1,52 @@
+package com.answer.service.Impl;
+
+import com.answer.service.QuestionService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Administrator on 2016/9/2.
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)  //spring和junit整合，junit启动时加载springIoc容器
+@ContextConfiguration({"classpath:spring/spring-*.xml"})  //告诉JUnitspring 的配置文件
+public class QuestionServiceImplTest {
+
+    @Autowired
+    QuestionService service ;
+
+    @Test
+    public void testInsert() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void testFindById() throws Exception {
+
+    }
+
+    @Test
+    public void testFindAll() throws Exception {
+          service.findAll();
+    }
+
+    @Test
+    public void testFindByPage() throws Exception {
+
+    }
+}
